4813793f8ad92ae8960628f232228bf3b3f6b127
vgupta038 <vgupta038@gmail.com>
2020-01-28 19:49:01 +0530

    version 1.0

---
+++ src/main/java/com/edufect/restdemo/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edufect.restdemo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Vishal
+ */
+@RestController
+@CrossOrigin(allowedHeaders="*", value="*")
+public class Controller {
+    
+    @GetMapping(value="/welcome")
+    public String welcome(){
+        return "welcome to edufect";
+    }
+    
+}
